0969be6e0ba2034293d111a37b94dc021774cc09
Sam <semkustikov@gmail.com>
2024-11-19 23:25:35 +0300

    initial

---
+++ BookClub-master/BookClub/urls.py
@@ -0,0 +1,27 @@
+"""BookClub URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("books/", include("books.urls")),
+
+    # Make sure to have users.urls above django.contrib.auth.urls so that custom auth views will
+    # take precedence over default ones (in this case, setting a custom login template)
+    path("users/", include("users.urls")),
+    path("users/", include("django.contrib.auth.urls")),
+]
